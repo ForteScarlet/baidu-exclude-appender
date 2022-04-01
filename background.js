959adfd97ff8d84d2a3f1be781447c94467e3469
ForteScarlet <ForteScarlet@163.com>
2022-04-01 23:45:50 +0800

    update..?

--- background.js
+++ background.js
@@ -1,6 +1,34 @@
 'use strict';
 chrome.runtime.onInstalled.addListener((details) => {
     console.log(details)
+
+    // web请求监听，最后一个参数表示阻塞式，需单独声明权限：webRequestBlocking
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        console.log(details)
+        // // cancel 表示取消本次请求
+        // if(!showImage && details.type == 'image') return {cancel: true};
+        // // 简单的音视频检测
+        // // 大部分网站视频的type并不是media，且视频做了防下载处理，所以这里仅仅是为了演示效果，无实际意义
+        // if(details.type == 'media') {
+        //     chrome.notifications.create(null, {
+        //         type: 'basic',
+        //         iconUrl: 'img/icon.png',
+        //         title: '检测到音视频',
+        //         message: '音视频地址：' + details.url,
+        //     });
+        // }
+    }, {urls: ["<all_urls>"]}, []);
+
+    console.log("In before request")
+
+    // web请求监听，最后一个参数表示阻塞式，需单独声明权限：webRequestBlocking
+    chrome.webRequest.onBeforeRedirect.addListener((details) => {
+        console.log(details)
+    }, {urls: ["<all_urls>"]}, []);
+
+    console.log("In before redirect")
+
 });
 
 console.log("H!")
+
--- content_scripts/search.js
+++ content_scripts/search.js
@@ -1,10 +1,47 @@
-console.log("baidu!")
-
-window.
-
-console.log(searchInput)
-searchInput.addEventListener('change', (event) => {
-    console.log("change")
-    console.log(event)
-})
-
+// console.log('!' + new Date())
+// const searchForm = document.getElementById('form')
+// if (searchForm) {
+//     const searchInput = document.getElementById('kw') // search input, name=wd
+//     const searchButton = document.getElementById('su') // button
+//     if (searchInput && searchButton) {
+//         const newRealInput = document.createElement('input')
+//         newRealInput.name = searchInput.name
+//         newRealInput.type = 'hidden'
+//         newRealInput.value = searchInput.value
+//         searchInput.removeAttribute('name') // .name = null
+//         searchInput.name = '__'
+//
+//
+//         console.log('newRealInput: ' + newRealInput.value)
+//         console.log(newRealInput)
+//         console.log('searchInput: ' + searchInput.value)
+//         console.log(searchInput)
+//
+//         searchForm.appendChild(newRealInput)
+//         console.log(searchForm)
+//
+//         // searchInput.addEventListener('change', (event) => {
+//         //     console.log("change")
+//         //     console.log(event)
+//         // })
+//
+//         searchForm.addEventListener('submit', (event) => {
+//             console.log('submit')
+//             console.log(event)
+//             newRealInput.value = searchInput.value + ' -csdn'
+//             console.log(event.submitter);
+//         })
+//     } else {
+//         console.log('searchInput && searchButton == false')
+//     }
+// } else {
+//     console.log('未找到 search form.')
+// }
+//
+//
+//
+//
+//
+//
+//
+//
