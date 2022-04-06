6e5d34b6e64ef8e7d0d1224192179ad76aaad2f9
ForteScarlet <ForteScarlet@163.com>
2022-04-07 00:12:32 +0800

    event?

--- content_scripts/search.js
+++ content_scripts/search.js
@@ -3,13 +3,31 @@ const searchForm = document.getElementById('form')
 if (searchForm) {
     const searchInput = document.getElementById('kw') // search input, name=wd
     if (searchInput) {
+        // searchInput.addEventListener('change',() => {
+        //     chrome.storage.sync.get(['excludes'], (res) => {
+        //         searchInput.value = resetSearch(searchInput.value, res.excludes.map((it) => it.id) || [])
+        //     })
+        // })
         searchForm.addEventListener('submit', () => {
             chrome.storage.sync.get(['excludes'], (res) => {
                 searchInput.value = resetSearch(searchInput.value, res.excludes.map((it) => it.id) || [])
                 searchInput.blur()
+                searchForm.submit()
             })
+            return false
         })
     }
+    // const searchBtn = document.getElementById('su')
+    // if (searchBtn) {
+    //     searchBtn.addEventListener('click', () => {
+    //         chrome.storage.sync.get(['excludes'], (res) => {
+    //             searchInput.value = resetSearch(searchInput.value, res.excludes.map((it) => it.id) || [])
+    //             searchInput.blur()
+    //             searchForm.submit()
+    //         })
+    //         return false
+    //     })
+    // }
     // else {
     //     console.log('searchInput && searchButton == false')
     // }
